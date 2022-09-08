05ddbd5fcccff83ab83139175c4dc012f1634d85
makssimomov <makssimonov96@gmail.com>
2022-09-08 12:17:25 +0500

    homework edit

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,18 @@
-let clicker__counter = document.getElementById("clicker__counter").innerHTML;
+// let clickerСounter = document.getElementById("clicker__counter").innerHTML;
+
+// cookie.onclick = function() {
+//     let cookieCount = ++clickerСounter;
+//     document.getElementById("clicker__counter").innerHTML = cookieCount;
+//     let cookie = document.getElementById("cookie");
+//     if (cookie.width <= 200) {
+//         cookie.width = 250;
+//     } else {
+//         cookie.width = 200;
+//     }
+// }
 
 cookie.onclick = function() {
-    let cookieCount = clicker__counter++;
-    document.getElementById("clicker__counter").innerHTML = cookieCount;
+    ++document.getElementById("clicker__counter").innerHTML;
     let cookie = document.getElementById("cookie");
-    if (cookie.width <= 200) {
-        cookie.width = 250;
-    } else {
-        cookie.width = 200;
-    }
+    cookie.width = cookie.width <= 200 ? 250 : 200;
 }
--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,13 +1,10 @@
 let hole = [...document.getElementsByClassName("hole")].forEach(item => { 
     item.addEventListener("click", () => {
-        // getHole = index => document.getElementById(`hole${index}`),
-        // deactivateHole = index => getHole( index ).className = "hole";
-        // activateHole = index => getHole( index ).className = "hole hole_has-mole";
         
         let deadCount = document.getElementById("dead").innerHTML;
         let lostCount = document.getElementById("lost").innerHTML;
         
-        if (document.getElementsByClassName("hole hole_has-mole") === document.getElementsByClassName("hole")) { 
+        if (item.className === "hole hole_has-mole") { 
             deadCount++;
             document.getElementById("dead").innerHTML = deadCount;
         } else {
